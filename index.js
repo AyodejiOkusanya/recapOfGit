2382d6fdd9022262db5501ef2b6e4159d86d2efb
AyodejiOkusanya <ayodeji.okusanya@gmail.com>
2024-06-03 18:04:11 +0800

    added some comments

---
+++ index.js
@@ -0,0 +1,10 @@
+// a new change to this file
+
+// working directory
+
+// staging area -- git add . , git add <name of the file>
+
+// local repo -- git commit -m "some message"
+
+// remote repo -- git push 
+
